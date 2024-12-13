7cb1704c29236a35b290acd13f6a020e485e6d66
stavzok <stav.zok@mail.huji.ac.il>
2024-12-13 13:51:01 +0200

    s

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 import ascii_art.AsciiArtAlgorithm;
 import ascii_output.ConsoleAsciiOutput;
+import ascii_output.HtmlAsciiOutput;
 import image.Image;
 
 public class main {
@@ -7,20 +8,20 @@ public class main {
     public static void main(String[] args) {
         Image image = null;
         try {
-            image = new Image("C:/Users/stav/IdeaProjects/OOP-EX3/examples/board.jpeg");
+            image = new Image("C:/Users/stav/IdeaProjects/OOP-EX3/examples/cat.jpeg");
         } catch (Exception e) {
             e.printStackTrace();
         }
 
 
-        char[] asciiChars = {'m', 'o'};
+        char[] asciiChars = {'0', '1', '2', '3', '4', '5', '6', '7','8','9'};
 
-        int resolution = 2;
+        int resolution = 128;
 
         AsciiArtAlgorithm algo = new AsciiArtAlgorithm(image, resolution, asciiChars);
         char[][] asciiImage = algo.run();
-
-        ConsoleAsciiOutput asciiOutput = new ConsoleAsciiOutput();
+        HtmlAsciiOutput asciiOutput = new HtmlAsciiOutput("C:/Users/stav/IdeaProjects/OOP-EX3/examples/catNew.jpeg",
+                "Courier New");
         asciiOutput.out(asciiImage);
 
 
